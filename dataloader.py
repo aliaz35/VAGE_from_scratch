52b29a8bc35dc20f7707b58d2bfcb41557b65452
aliaz35 <1742522371@qq.com>
2024-12-01 19:20:46 +0800

    refactor: load raw data with pkl_load or direct_load

--- dataloader.py
+++ dataloader.py
@@ -1,74 +1,73 @@
 import torch
 import numpy as np
-from abc import abstractmethod, ABC
+import scipy
 import pickle as pkl
 import networkx as nx
+from jedi.inference.gradual.typing import Callable
 
-class Load(ABC):
-    def __init__(self):
-        super().__init__()
 
-    @abstractmethod
-    def load(self, dataset: str, extension: str):
-        pass
+def direct_load(
+        dataset: str,
+        extension: str
+    ) -> list[int]:
+    with open(f"data/ind.{dataset}.{extension}", "r") as f:
+        return [int(line) for line in f]
 
-class DirectLoad(Load):
-    def __init__(self):
-        super().__init__()
+def pkl_load(
+        dataset: str,
+        extension: str,
+        wrapper: callable=None
+    ) ->scipy.sparse.csr_matrix | torch.Tensor:
+    with open(f"data/ind.{dataset}.{extension}", "rb") as f:
+        ret = pkl.load(f, encoding="latin1")
+        return ret if wrapper is None else wrapper(ret)
 
-    def load(self, dataset: str, extension: str):
-        with open(f"data/ind.{dataset}.{extension}", "r") as f:
-            return [int(line) for line in f]
 
-class PklLoad(Load):
-    def __init__(self):
-        super().__init__()
+def _graph_wrapper(x) -> torch.Tensor:
+    A = nx.adjacency_matrix(nx.from_dict_of_lists(x))
+    return torch.sparse_coo_tensor(
+        np.array(A.nonzero()),
+        A.data,
+        A.shape
+    )
 
-    def load(self, dataset: str, extension: str):
-        with open(f"data/ind.{dataset}.{extension}", "rb") as f:
-            return pkl.load(f, encoding="latin1")
 
-class LoadX(PklLoad):
-    def __init__(self):
-        super().__init__()
-
-    def _load_wrapper(self, dataset: str, extension: str) -> torch.Tensor:
-        x = self.load(dataset, extension)
-        return torch.sparse_coo_tensor(np.array(x.nonzero()), x.data, x.shape)
-
-    def __call__(
-            self,
-            dataset: str,
-    ) -> dict[str, torch.sparse_coo_tensor]:
-        return {
-            "x"     : self._load_wrapper(dataset, "x"),
-            "tx"    : self._load_wrapper(dataset, "tx"),
-            "allx"  : self._load_wrapper(dataset, "allx"),
-        }
-
-class LoadAdj(PklLoad):
-    def __init__(self):
-        super().__init__()
-
-    def __call__(self, dataset: str) -> torch.sparse_coo_tensor:
-        A = nx.adjacency_matrix(nx.from_dict_of_lists(self.load(dataset, "graph")))
-        return {"A" : torch.sparse_coo_tensor(
-            np.array(A.nonzero()),
-            A.data,
-            A.shape
-        )}
-
-class LoadIndices(DirectLoad):
-    def __init__(self):
-        super().__init__()
-
-    def __call__(self, dataset: str, extension: str) -> list[int]:
-        return self.load(dataset, extension)
-
-class Dataset(LoadX, LoadAdj):
-    def __init__(self, dataset: str):
-        super().__init__()
-        self._data = LoadX.__call__(self, dataset) | LoadAdj.__call__(self, dataset)
-
-    def extract(self):
-        return self._data
+def load_raw_data(dataset) -> dict:
+    return {
+        "x": pkl_load(
+            dataset,
+            "x",
+            lambda x: torch.sparse_coo_tensor(
+                np.array(x.nonzero()),
+                x.data,
+                x.shape
+            )
+        ),
+        "tx": pkl_load(
+            dataset,
+            "tx",
+            lambda x: torch.sparse_coo_tensor(
+                np.array(x.nonzero()),
+                x.data,
+                x.shape
+            )
+        ),
+        "allx": pkl_load(
+            dataset,
+            "allx",
+            lambda x: torch.sparse_coo_tensor(
+                np.array(x.nonzero()),
+                x.data,
+                x.shape
+            )
+        ),
+        "graph": pkl_load(
+            dataset,
+            "graph",
+            _graph_wrapper
+        ),
+        "index": direct_load(
+            dataset,
+            "test.index"
+        )
+    }
