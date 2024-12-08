feef5be43183542d60b259c4fbadea7a1296de71
aliaz35 <1742522371@qq.com>
2024-12-08 20:56:33 +0800

    refactor(ModelTrainer): proxy three trainning stage(train, validate, test)
    - rename train(), valid(), test() into train_epoch(), validate_epoch(),
    test_epoch() to represent progress more explicitly
    - convert BCELoss into lambda expression to be consistent with kl
    divergence

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 
 from torch.nn import BCELoss
 from torch.optim import Adam
+from torch.nn.functional import binary_cross_entropy
 from model import VGAE
 from utils import parse_cmdline
 from sklearn.metrics import roc_auc_score, average_precision_score
@@ -15,40 +16,50 @@ from sklearn.metrics import roc_auc_score, average_precision_score
 class Loss(nn.Module):
     def __init__(self, train_graph: dgl.DGLGraph):
         super().__init__()
-        g = train_graph.remove_self_loop()
-        all_edges_count = g.num_nodes() * g.num_nodes()
-        self.bce_norm = all_edges_count / ((all_edges_count - g.num_edges()) * 2)
-        pos_weight = (all_edges_count - g.num_edges()) / g.num_edges()
-        self.bce_weight = torch.ones(all_edges_count)
-        self.bce_weight[train_graph.adjacency_matrix().to_dense().view(-1) == 1] *= pos_weight
-        self.kl_norm = 1 / g.num_nodes()
-        self._bce_loss = BCELoss(weight=self.bce_weight)
+        _g = train_graph.remove_self_loop()
+        _all_edges_count = _g.num_nodes() * _g.num_nodes()
+
+        _bce_norm = _all_edges_count / ((_all_edges_count - _g.num_edges()) * 2)
+        _bce_weight = torch.ones(_all_edges_count)
+        _bce_weight[train_graph.adjacency_matrix().to_dense().view(-1) == 1] \
+            *= (_all_edges_count - _g.num_edges()) / _g.num_edges()
+        self._bce_loss = lambda predictions, labels: \
+            _bce_norm * binary_cross_entropy(
+                predictions,
+                labels,
+                weight=_bce_weight
+            )
+
+        _kl_norm = 1 / _g.num_nodes()
         self._kl_divergence = \
-            lambda mu, log_sigma: 0.5 * (1 + 2 * log_sigma - mu ** 2 - torch.exp(log_sigma) ** 2).sum(1).mean()
+            lambda mu, log_sigma: \
+                (_kl_norm * 0.5 * (1 + 2 * log_sigma - mu ** 2 - torch.exp(log_sigma) ** 2)
+                 .sum(1)
+                 .mean())
 
     def forward(self,
                 predictions: torch.Tensor,
                 labels: torch.Tensor,
                 mu: torch.Tensor,
                 log_sigma: torch.Tensor
-                ) -> torch.Tensor:
-        return (self.bce_norm * self._bce_loss(predictions, labels)
-                - self.kl_norm * self._kl_divergence(mu, log_sigma))
+    ) -> torch.Tensor:
+        return (self._bce_loss(predictions, labels)
+                - self._kl_divergence(mu, log_sigma))
 
-class RunProxy:
+class ModelTrainer:
     def __init__(self, args: argparse.Namespace) -> None:
         torch.manual_seed(args.seed)
         random.seed(args.seed)
+
         self.dataloader = DataLoader(args)
         self.dataset = self.dataloader.load()
         self.model  = VGAE(in_feats=self.dataset.features.shape[1],
                            hidden_feats=args.hidden_feats,
                            out_feats=args.out_feats).to(args.device)
-        self.loss = Loss(self.dataset.train_graph).to(args.device)
+        self.criteria = Loss(self.dataset.train_graph).to(args.device)
         self.optimizer = Adam(self.model.parameters(), lr=args.lr)
-        self.train_loss = None
-        self.auc = {}
-        self.ap = {}
+
+        self.loss = None
 
     def _evaluate(self, edges, A_hat: torch.Tensor) -> tuple[float, float]:
         y_score = []
@@ -60,42 +71,45 @@ class RunProxy:
         return roc_auc_score(y_true, y_score),\
             average_precision_score(y_true, y_score)
 
-    def train(self) -> tuple[torch.Tensor, torch.Tensor]:
+    def train_epoch(self) -> tuple[torch.Tensor, torch.Tensor]:
         self.model.train()
         A_hat, bottleneck = self.model(self.dataset.train_graph, self.dataset.features)
         d = self.model.distribution()
         self.optimizer.zero_grad()
-        self.train_loss = self.loss(A_hat.view(-1),
-                         self.dataset.train_graph.adjacency_matrix().to_dense().view(-1).float(),
-                         d.mu,
-                         d.log_sigma)
-        self.train_loss.backward()
+        self.loss = self.criteria(A_hat.view(-1),
+                                  self.dataset.train_graph.adjacency_matrix().to_dense().view(-1).float(),
+                                  d.mu,
+                                  d.log_sigma)
+        self.loss.backward()
         self.optimizer.step()
         return A_hat, bottleneck
 
     @torch.no_grad()
-    def valid(self, A_hat: torch.Tensor) -> None:
+    def validate_epoch(self, A_hat: torch.Tensor) -> None:
         self.model.eval()
         auc, ap = self._evaluate(self.dataset.valid_edges, A_hat)
-        self.auc["valid"] = auc
-        self.ap["valid"] = ap
+        print(
+            f"Epoch {epoch:03d} | "
+            f"Train Loss: {trainer.loss:.4f} | "
+            f"Valid auc: {auc:.4f} | "
+            f"Valid ap: {ap:.4f}"
+        )
 
     @torch.no_grad()
-    def test(self, A_hat: torch.Tensor) -> None:
+    def test_epoch(self, A_hat: torch.Tensor) -> None:
         self.model.eval()
         auc, ap = self._evaluate(self.dataset.test_edges, A_hat)
-        self.auc["test"] = auc
-        self.ap["test"] = ap
-
+        print(
+            f"Test auc: {auc:.4f} | "
+            f"Test ap: {ap:.4f}"
+        )
 
 if __name__ == "__main__":
     args = parse_cmdline()
-    proxy = RunProxy(args)
+    trainer = ModelTrainer(args)
     A_hat = None
     bottleneck = None
     for epoch in range(0, 200):
-        A_hat, bottleneck = proxy.train()
-        proxy.valid(A_hat)
-        print(f"epoch: {epoch}, train_loss: {proxy.train_loss}, valid auc: {proxy.auc["valid"]}, valid ap: {proxy.ap["valid"]}")
-    proxy.test(A_hat)
-    print(f"test_auc: {proxy.auc["test"]}, test_ap: {proxy.ap["test"]}")
+        A_hat, bottleneck = trainer.train_epoch()
+        trainer.validate_epoch(A_hat)
+    trainer.test_epoch(A_hat)
